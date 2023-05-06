8e4034173f3118b97481c4a6040c2defa69e4be7
dalesnoy <103219005+dalesnoy@users.noreply.github.com>
2023-05-06 19:27:25 +0300

    Add files via upload

---
+++ что такое break/что такое break.cpp
@@ -0,0 +1,38 @@
+﻿//Ключевое слово break, заканчивает цикл, даже если по условию действие может выполняться.
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	setlocale(0, "");
+
+	cout << "Начало цикла" << endl;
+	/*
+	for (int i = 0; ; i++)
+	{
+		cout << "Переменная i = " << i << endl;
+		if (i == 5)
+		{
+			break;
+		}
+	}
+	*/
+	int i = 0;
+	while (true)
+	{
+		cout << "Переменная i = " << i << endl;
+		i++;
+		if (i == 6)
+		{
+			break;
+		}
+
+	}
+
+	cout << "конец цикла\n";
+}
